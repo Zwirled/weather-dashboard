799f446dea925dad43d390b28c2068b90d2bc8d6
Zwirled <bramwellamelia@gmail.com>
2023-02-04 10:27:04 +0000

    Tidied JS and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,11 @@ searchBtn.on('click', function (event) {
     // Get the user-inputted value
     let city = $('#search-input').val().trim();
     // Query URL + City + API
-    let geoQueryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey;
+    let geoQueryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + apiKey;
 
     $.ajax({
         url: geoQueryURL,
-        method: "GET"
+        method: 'GET'
     }).then(function (response) {
 
         // Get the latitude as a variable to 2 decimal places
@@ -20,26 +20,30 @@ searchBtn.on('click', function (event) {
         // Get the longitude as a variable to 2 decimal places
         let longitude = response[0].lon.toFixed(2);
         // Query URL + Lat + Long + API Key
-        let weatherQueryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=" + apiKey;
-
-        console.log(weatherQueryURL);
+        let weatherQueryURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=' + apiKey;
 
         $.ajax({
             url: weatherQueryURL,
-            method: "GET"
+            method: 'GET'
         }).then(function (response) {
 
+            // Get the city name
             let name = $('<h2>').text(response.city.name);
+            // Add it to the page
             $('#today').append(name);
-            let temp = $('<p>').text("Temperature: " + response.list[0].main.temp + "°C");
+            // Get the temp
+            let temp = $('<p>').text('Temperature: ' + response.list[0].main.temp + '°C');
+            // Add it to the page
             $('#today').append(temp);
-            let wind = $('<p>').text("Wind: " + response.list[0].wind.speed + "KPH");
+            // Get the wind speed
+            let wind = $('<p>').text('Wind: ' + response.list[0].wind.speed + 'KPH');
+            // Add it to the page
             $('#today').append(wind);
-            let humidity = $('<p>').text("Humidity: " + response.list[0].main.humidity + "%");
+            // Get the humidity
+            let humidity = $('<p>').text('Humidity: ' + response.list[0].main.humidity + '%');
+            // Add it to the page
             $('#today').append(humidity);
-
         })
     })
-
 
 });
