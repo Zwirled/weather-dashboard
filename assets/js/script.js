c743e5b3f8efde014fb6375798c6eefac9f79c17
Zwirled <bramwellamelia@gmail.com>
2023-02-05 09:51:59 +0000

    Local storage added but only saving 1 location so far

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,50 +27,86 @@ searchBtn.on('click', function (event) {
             method: 'GET'
         }).then(function (response) {
 
-            /////
-            // LOCAL STORAGE BUTTONS
-            /////
-
             let location = $('<button>').text(response.city.name);
             $('#history').prepend(location);
+            getTemperatures(response);
+            localStorage.setItem('location', location.text());
 
             location.on('click', function () {
-                $('#today').empty();
-                // Get the city name
-                let name = $('<h2>').text(response.city.name);
-                $('#today').append(name);
-                // Get the temp
-                let temp = $('<p>').text('Temperature: ' + response.list[0].main.temp.toFixed(2) + '°C');
-                $('#today').append(temp);
-                // Get the wind speed
-                let wind = $('<p>').text('Wind: ' + (response.list[0].wind.speed * 2.237).toFixed(2) + 'mph');
-                $('#today').append(wind);
-                // Get the humidity
-                let humidity = $('<p>').text('Humidity: ' + response.list[0].main.humidity + '%');
-                $('#today').append(humidity);
-            })
+                getTemperatures(response);
+            });
 
-            /////
-            // TODAY WEATHER INFO //
-            /////
-
-            // Get the city name
-            $('#today').empty();
-            let name = $('<h2>').text(response.city.name);
-            $('#today').append(name);
-            // Get the temp
-            let temp = $('<p>').text('Temperature: ' + response.list[0].main.temp.toFixed(2) + '°C');
-            $('#today').append(temp);
-            // Get the wind speed
-            let wind = $('<p>').text('Wind: ' + (response.list[0].wind.speed * 2.237).toFixed(2) + 'mph');
-            $('#today').append(wind);
-            // Get the humidity
-            let humidity = $('<p>').text('Humidity: ' + response.list[0].main.humidity + '%');
-            $('#today').append(humidity);
-
-            /////
-            // 5 DAY FORECAST
-            /////
         })
     })
 });
+
+$(document).ready(function () {
+
+    // Get the location from local storage if it exists
+    let location = localStorage.getItem('location');
+
+    // If local storage item exists...
+    if (location) {
+        // add the saved location to the button
+        let savedLocation = $('<button>').text(location);
+        // prepend the saved location to the div
+        $('#history').prepend(savedLocation);
+
+        // when saved location is clicked...
+        savedLocation.on('click', function () {
+
+            // get the location
+            let city = location;
+            // add the city + API to the query url
+            let geoQueryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=' + apiKey;
+
+            $.ajax({
+                url: geoQueryURL,
+                method: 'GET'
+            }).then(function (response) {
+
+                // Get the latitude as a variable to 2 decimal places
+                let latitude = response[0].lat.toFixed(2);
+                // Get the longitude as a variable to 2 decimal places
+                let longitude = response[0].lon.toFixed(2);
+                // Query URL + Lat + Long + API Key
+                let weatherQueryURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=' + apiKey;
+
+                $.ajax({
+                    url: weatherQueryURL,
+                    method: 'GET'
+                }).then(function (response) {
+                    getTemperatures(response);
+                });
+            });
+        });
+    }
+});
+
+// Function to get all the temperatures for a location
+function getTemperatures(response) {
+    // Get the city name
+    $('#today').empty();
+    $('#forecast').empty();
+    let name = $('<h2>').text(response.city.name);
+    // Get the temp
+    let temp = $('<p>').text('Temperature: ' + response.list[0].main.temp.toFixed(2) + '°C');
+    // Get the wind speed
+    let wind = $('<p>').text('Wind: ' + (response.list[0].wind.speed * 2.237).toFixed(2) + 'mph');
+    // Get the humidity
+    let humidity = $('<p>').text('Humidity: ' + response.list[0].main.humidity + '%');
+    $('#today').append(name, temp, wind, humidity);
+
+    for (let i = 1; i < 6; i++) {
+        let column = $('<div>').attr('class', 'col');
+        let name = $('<h2>').text(response.city.name);
+        // Get the temp
+        let temp = $('<p>').text('Temperature: ' + response.list[i].main.temp.toFixed(2) + '°C');
+        // Get the wind speed
+        let wind = $('<p>').text('Wind: ' + (response.list[i].wind.speed * 2.237).toFixed(2) + 'mph');
+        // Get the humidity
+        let humidity = $('<p>').text('Humidity: ' + response.list[i].main.humidity + '%');
+        $('#forecast').append(column);
+        $(column).append(name, temp, wind, humidity);
+    }
+}
